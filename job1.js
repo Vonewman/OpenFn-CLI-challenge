62866ffa5442934c81419b7fd402d501dc6ee1dc
vonewman <vonewman7@gmail.com>
2023-08-23 00:13:34 +0000

    Challenge: Reduce, filter, and map

---
+++ job1.js
@@ -0,0 +1,28 @@
+// Get all posts
+get('posts');
+
+// Filter posts by user id
+fn(state => {
+  const posts = state.data;
+
+  // Define the user id for filtering
+  const userIdToFilter = 1;
+
+  // Use Array.filter to get posts for the specified user id
+  const filteredPosts = posts.filter(post => post.userId === userIdToFilter);
+
+  // Return the filtered posts
+  return { state, filteredPosts };
+});
+
+// Log the filtered posts
+fn(state => {
+  const { filteredPosts } = state;
+
+  // Log each filtered post
+  filteredPosts.forEach(post => {
+    console.log('Filtered Post', JSON.stringify(post, null, 2));
+  });
+
+  return state;
+});

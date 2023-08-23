9a3fff1091b9cc6b08ab29d4d5f98fb31eb57148
vonewman <vonewman7@gmail.com>
2023-08-23 15:23:33 +0000

    remove the comments from covid19.js

--- covid19.js
+++ covid19.js
@@ -1,14 +1,7 @@
-// Fetch historical data for all days
 get('historical/all?lastdays=all');
 
-// Process and log the fetched data
 fn(state => {
-  // Extract the fetched data from the state object
   const posts = state.data;
-
-  // Log the fetched data (historical data for all days)
   console.log(posts);
-
-  // Return the state object unchanged
   return state;
 });
